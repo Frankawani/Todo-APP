9f3cfb40c14b08c6d4366b7891219a2f17dca6c8
Tania <tania@MacBook-Pro-de-nico.local>
2022-08-29 11:41:16 +0200

    added personalized styles and background image of the application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-
+import "./App.css";
+import iconSun from './assets/images/icon-sun.svg'
 function App() {
   return (
-    <div className="text-3xl underline">
-      hello toi
+    <div className="w-full h-screen">
+      <div className="w-full h-1/4 bg-desktopDarkTheme bg-cover relative">
+        <div className="w-1/3 sm:w-2/3 h-64 absolute top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="flex justify-between align-center">
+            <h1 className="text-white font-bold text-2xl tracking-[.55em]">TODO</h1>
+            <button><img src={iconSun} alt="icon sun"/></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,39 @@ module.exports = {
     "./src/**/*.{js, jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': 'hsl(220, 98%, 61%)',
+        'check-background': 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+
+      },
+      fontFamily: {
+        'josefin': 'Josefin Sans, sans-serif'
+      },
+      backgroundImage: {
+        'desktopDarkTheme': 'url(/src/assets/images/bg-desktop-dark.jpg)',
+        'desktopLightTheme': 'url(/src/assets/images/bg-desktop-light.jpg)',
+        'mobileDarkTheme': 'url(/src/assets/images/bg-mobile-light.jpg)',
+        'mobileLightTheme': 'url(/src/assets/images/bg-mobile-light.jpg)',
+      },
+      screens: {
+        '3xl': {'max': '2500px'},
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '967px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '569px'},
+        // => @media (max-width: 639px) { ... }
+      },
+    },
   },
   plugins: [],
 }
