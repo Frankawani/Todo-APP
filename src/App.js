8bd9c78a0f0bc80061d3157e9183bc5015f9d672
Tania <tania@MacBook-Pro-de-nico.local>
2022-08-29 13:11:06 +0200

    added combine reducers functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Header from "./components/Header";
-import { useSelector, useDispatch } from 'react-redux'
-
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  const isLight = useSelector(state => state.isLight);
-  
+  const { isLight } = useSelector((state) => ({
+    ...state.ThemeReducer,
+  }));
+
   return (
     <div className={`w-full h-screen ${isLight ? "bg-white" : "bg-black"}`}>
       <div className="w-full h-1/4 bg-desktopDarkTheme bg-cover relative">
         <div className="w-1/3 sm:w-2/3 h-64 absolute top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Header/>
+          <Header />
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import ThemeReducer from "./Reducers/ThemeReducer";
 
-const Store = createStore(ThemeReducer);
+const rootReducer = combineReducers({
+  ThemeReducer,
+})
+
+const Store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
